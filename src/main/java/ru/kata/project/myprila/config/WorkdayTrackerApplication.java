ac5dbd960d4739fc9f1ab947113ffcf27741d08d
MikhailTolstykh <tolstich_m@mail.ru>
2025-10-28 21:31:16 +0700

    "Switch to PostgreSQL and update dependencies"

--- src/main/java/ru/kata/project/myprila/config/WorkdayTrackerApplication.java
+++ src/main/java/ru/kata/project/myprila/config/WorkdayTrackerApplication.java
@@ -2,8 +2,12 @@ package ru.kata.project.myprila.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "ru.kata.project.myprila.repo")
+@EntityScan(basePackages = "ru.kata.project.myprila.entity")
 public class WorkdayTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorkdayTrackerApplication.class, args);
